cd42044869876b6af71ec0ddd907641820aae4e8
MAMUN-XD-GOD <info.trader7x@gmail.com>
2026-01-11 11:32:32 +0600

    Create market_detector.py

---
+++ core/market_detector.py
@@ -0,0 +1,7 @@
+def detect_market(img):
+    # Phase-1 focused on Binary / Pocket Option
+    return {
+        "type": "BINARY",
+        "platform": "Pocket Option",
+        "mode": "REAL / OTC AUTO"
+    }
