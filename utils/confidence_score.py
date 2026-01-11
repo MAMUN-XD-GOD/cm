9a85a6b6049bafa3167bcfb54b53f97ae9df53a9
MAMUN-XD-GOD <info.trader7x@gmail.com>
2026-01-11 11:50:42 +0600

    Update confidence_score.py

--- utils/confidence_score.py
+++ utils/confidence_score.py
@@ -1,14 +1,16 @@
+from core.smc import detect_fvg, detect_order_block
+
 def confidence_score(candles, structure):
-    score = 30
+    score = 35
     last = candles[-1]
 
-    if last["strong"]:
+    if detect_fvg(candles):
         score += 20
 
-    if not last["rejection"]:
+    if detect_order_block(candles):
+        score += 20
+
+    if last["strong"]:
         score += 15
 
-    if "BREAK_HOLD" in structure:
-        score += 25
-
-    return min(score, 97)
+    return min(score, 98)
